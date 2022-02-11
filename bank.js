d75918dac5693866a4f29fe793f282cb0aaade81
ismailmozumdar <ismailmozumdar770@gmail.com>
2022-02-11 23:55:43 +0600

    this dat stop

--- bank.js
+++ bank.js
@@ -5,10 +5,44 @@
 // --------------------Deposit-------------------
 document.getElementById('Deposit-btn').addEventListener('click', function(){
     const depositInput = document.getElementById('Deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const deposittext = depositInput.value;
+    const depositAmount = parseFloat(deposittext);
+
+    // updet Deposit total
 
     const Deposittotal = document.getElementById('Deposit-total');
-    Deposittotal.innerText = depositAmount;
+    const correntDeposittext = parseFloat(Deposittotal.innerText);
+    const newDepositTotal = correntDeposittext + depositAmount;
+    Deposittotal.innerText = newDepositTotal;
+
+    // updet account balence
+    const Blancetotal = document.getElementById('Blance-total');
+    const correntBlancetotal = parseFloat(Blancetotal.innerText);
+    const newBlancetotal = correntBlancetotal + depositAmount;
+    Blancetotal.innerText = newBlancetotal;
+
     depositInput.value = '';
+})
+
+// handel Event hendelar
+document.getElementById('Withdraw-btn').addEventListener('click', function(){
+    const WithdrawInput = document.getElementById('Withdraw-input');
+    const WithdrawAmounttext = WithdrawInput.value;
+    const NewWithdrawAmount = parseFloat(WithdrawAmounttext);
+
+    // set withdrow Amount
+    const Withdrawtotal = document.getElementById('Withdraw-total');
+    const preveoWithdrawtotaltext = Withdrawtotal.innerText;
+    const preveoWithdrawtotal = parseFloat(preveoWithdrawtotaltext);
+
+    const NewpreveoWithdrawtotal = NewWithdrawAmount + preveoWithdrawtotal;
+    Withdrawtotal.innerText = NewpreveoWithdrawtotal;
+
+     // updet account balence
+     const Blancetotal = document.getElementById('Blance-total');
+     const correntBlancetotal = parseFloat(Blancetotal.innerText);
+     const newBlancetotal = correntBlancetotal - NewWithdrawAmount;
+     Blancetotal.innerText = newBlancetotal;
+
+    WithdrawInput.value = '';
 })
