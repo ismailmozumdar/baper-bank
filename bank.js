94f079ce04dd61813df22288e7e475d3edae1126
ismailmozumdar <ismailmozumdar770@gmail.com>
2022-02-11 22:49:16 +0600

    complete

--- bank.js
+++ bank.js
@@ -5,19 +5,10 @@
 // --------------------Deposit-------------------
 document.getElementById('Deposit-btn').addEventListener('click', function(){
     const depositInput = document.getElementById('Deposit-input');
-    const depositAmount = depositInput.value
+    const depositAmount = depositInput.value;
     console.log(depositAmount);
 
     const Deposittotal = document.getElementById('Deposit-total');
     Deposittotal.innerText = depositAmount;
     depositInput.value = '';
-})
-
-
-
-// --------------Withdraw--------------------
-document.getElementById('Withdraw-btn').addEventListener('click', function(){
-    const WithdrawInput = document.getElementById('Withdraw-input');
-    const WithdrawAmount = WithdrawInput.value
-    console.log(WithdrawAmount);
 })
